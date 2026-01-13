93306739b7ee40f1e2f0cea231ccfa4f4118047a
xlp67 <121883066+xlp67@users.noreply.github.com>
2026-01-13 11:41:32 -0300

    Delete helm/install.sh

--- helm/install.sh
+++
@@ -1,3 +0,0 @@
-k3d cluster create --api-port 6550 -p "8081:80@loadbalancer" --agents 2 --volume /home/thiago/Documentos/Code/airflow-pyspark/dags:/var/lib/dags@all
-
-helm install airflow apache-airflow/airflow --namespace airflow --create-namespace --set postgresql.image.tag="latest" -f values.yaml
